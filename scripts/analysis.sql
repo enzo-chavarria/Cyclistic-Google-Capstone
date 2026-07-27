3d11270e22f5413a2e68aa8e74167546f4d9dae7
enzo-chavarria <enzochav99@gmail.com>
2026-07-27 12:40:05 -0700

    day of week comparison

--- scripts/analysis.sql
+++ scripts/analysis.sql
@@ -7,3 +7,21 @@ SELECT
   COUNT(*) AS num_rides
 FROM `CyclisticData.trips_combined`
 GROUP BY member_casual;
+
+--Compare Ride Count by Day of Week and Rider Type
+SELECT
+  member_casual,
+  day_of_week,
+  CASE day_of_week
+    WHEN 1 THEN 'Sunday'
+    WHEN 2 THEN 'Monday'
+    WHEN 3 THEN 'Tuesday'
+    WHEN 4 THEN 'Wednesday'
+    WHEN 5 THEN 'Thursday'
+    WHEN 6 THEN 'Friday'
+    WHEN 7 THEN 'Saturday'
+  END AS day_name,
+  COUNT(*) AS num_rides
+FROM `CyclisticData.trips_combined`
+GROUP BY member_casual, day_of_week
+ORDER BY member_casual, day_of_week;
